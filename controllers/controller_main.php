f78efebc8eb07a12df1babf43acda81ff8ec7861
Maxim Vlasov <sergeich06@gmail.com>
2016-03-23 08:36:37 +0300

    Build UI

--- controllers/controller_main.php
+++ controllers/controller_main.php
@@ -6,7 +6,6 @@ class Controller_Main extends Controller
         $this->view->generate('header.php',['title'=>'File uploads']);
         $this->view->generate('panel.php');
         $this->view->generate('table.php');
-        $this->view->generate('main.php',['test'=>12234234]);
         $this->view->generate('footer.php');
 
     }
