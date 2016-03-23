710d4461ebe65bdf8554777e7164e60a74c1374b
tr0yka <sergeich06@gmail.com>
2016-03-23 16:01:35 +0300

    Added Admin function

--- views/filter.php
+++ views/filter.php
@@ -5,5 +5,8 @@
         <td><?=$elem['fileSize']?></td>
         <td><?=$elem['added']?></td>
         <td><? echo substr(htmlspecialchars_decode($elem['description']), 0, 200);?></td>
+        <? if($_SERVER['REMOTE_ADDR'] = '127.0.0.1'): ?>
+            <td><a href="/main/delete/?file=<?=$elem['id'];?>">Удалить</a></td>
+        <? endif; ?>
     </tr>
 <? endforeach; ?>
