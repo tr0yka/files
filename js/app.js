00229ba10c6cb059cf5239d99296691c56da4c77
tr0yka <sergeich06@gmail.com>
2016-03-23 10:19:44 +0300

    Add Sort and Filter

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('#add_btn').click(function(){
+        $('#popup').css({"display":"block"});
+    });
+    $('#close').click(function(){
+        $('#popup').css({"display":"none"});
+    });
+    $("table th").click(function(){
+        console.log($(this).attr('id'));
+    });
+    $('#file_list').tablesorter();
+    $('#filter input').keydown(function(e){
+        if(e.keyCode == 13){
+            getQuery($(this).val())
+        }
+
+    });
+});
+
+function getQuery(query){
+    $.get('/main/filter/',{filter:query}).done(function(data){
+        $('#file_list tbody').html(data);
+    });
+}
